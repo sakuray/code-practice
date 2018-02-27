58ec5d9a4238f2610cc7d1764ed8a5e0bd143808
sakuray <luowentao1454@gmail.com>
2018-02-27 19:58:02 +0800

    添加3题

---
+++ src/main/java/com/sakuray/code/practice/leetcode/_007_ReverseInteger.java
@@ -0,0 +1,69 @@
+package com.sakuray.code.practice.leetcode;
+
+import java.util.Stack;
+
+/**
+ * Given a 32-bit signed integer, reverse digits of an integer.
+	Example 1:
+		Input: 123
+		Output:  321
+	Example 2:
+		Input: -123
+		Output: -321
+	Example 3:
+		Input: 120
+		Output: 21
+ *	Note:
+ *	Assume we are dealing with an environment which could only hold integers within the 32-bit signed integer range. 
+ *	For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+ *
+ */
+public class _007_ReverseInteger {
+
+	public static void main(String[] args) {
+		System.out.println(321 == reverse(123));
+		System.out.println(-321 == reverse(-123));
+		System.out.println(21 == reverse(120));
+		System.out.println(654321 == reverse(123456));
+		System.out.println(0 == reverse(1534236469));
+		System.out.println(0 == reverse(-2147483648));
+		System.out.println(0 == reverse(1563847412));
+	}
+	
+	public static int reverse(int x) {
+		if(x > Integer.MAX_VALUE || x < Integer.MIN_VALUE) return 0;
+        Stack<Integer> bit = new Stack<>();
+        while(x != 0) {
+        	bit.push(x % 10);
+        	x = x / 10;
+        }
+        int size = bit.size();
+        int index = 1;
+        int under = -1;
+        for(int i = 0; i < size; i++) {
+        	if(i != 0) index *= 10;
+        	under = bit.pop();
+        	if(index >= 0x10000000 &&  (under > 2 || under < -2  || 
+        			(under == 2 && x > Integer.MAX_VALUE - 2000000000) || 
+        			(under == -2 && x < Integer.MIN_VALUE + 2000000000))) {
+        		return 0;	// 溢出
+        	}
+        	x += under * index;
+        }
+		return x;
+    }
+	
+	public static int reverse_S(int x) {
+	    int result = 0;
+	    while (x != 0) {
+	        int tail = x % 10;
+	        int newResult = result * 10 + tail;
+	        if ((newResult - tail) / 10 != result) { 
+	        	return 0; 
+	        }
+	        result = newResult;
+	        x = x / 10;
+	    }
+	    return result;
+	}
+}
---
+++ src/main/java/com/sakuray/code/practice/leetcode/_009_PalindromeNumber.java
@@ -0,0 +1,59 @@
+package com.sakuray.code.practice.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Determine whether an integer is a palindrome. Do this without extra space.
+ * Could negative integers be palindromes? (ie, -1)
+ * If you are thinking of converting the integer to string, note the restriction of using extra space.
+ * You could also try reversing an integer. However, if you have solved the problem "Reverse Integer", 
+ * you know that the reversed integer might overflow. How would you handle such case?
+ * There is a more generic way of solving this problem.
+ */
+public class _009_PalindromeNumber {
+	
+	public static void main(String[] args) {
+		System.out.println(isPalindrome(111));
+		System.out.println(isPalindrome(21112));
+		System.out.println(isPalindrome(1111));
+		System.out.println(isPalindrome(-2147447412));
+	}
+
+	public static boolean isPalindrome(int x) {
+		if(x < 0 ) return false;
+		List<Integer> bit = new ArrayList<>();
+		while(x != 0) {
+			bit.add(x % 10);
+			x = x / 10;
+		}
+		for(int i = 0; i < bit.size() / 2; i++) {
+			if(bit.get(i) != bit.get(bit.size() - i - 1)) {
+				return false;
+			}
+		}
+        return true;
+    }
+	
+	public static boolean IsPalindrome(int x) {
+        // Special cases:
+        // As discussed above, when x < 0, x is not a palindrome.
+        // Also if the last digit of the number is 0, in order to be a palindrome, 
+        // the first digit of the number also needs to be 0.
+        // Only 0 satisfy this property.
+        if(x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        
+        int revertedNumber = 0;
+        while(x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        
+        // When the length is an odd number, we can get rid of the middle digit by revertedNumber/10
+        // For example when the input is 12321, at the end of the while loop we get x = 12, revertedNumber = 123, 
+        // since the middle digit doesn't matter in palidrome(it will always equal to itself), we can simply get rid of it.
+        return x == revertedNumber || x == revertedNumber/10;
+    }
+}
