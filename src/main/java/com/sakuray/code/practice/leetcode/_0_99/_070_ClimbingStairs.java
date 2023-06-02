ffc6b3670cc7045872c5982227a7b3cfa3e7eea3
sakuray <luowentao1454@gmail.com>
2023-06-02 16:53:17 +0800

    【简】动态规划：跳方格游戏，步长可能性固定2种

---
+++ src/main/java/com/sakuray/code/practice/leetcode/_0_99/_070_ClimbingStairs.java
@@ -0,0 +1,37 @@
+package com.sakuray.code.practice.leetcode._0_99;
+
+/**
+ * You are climbing a staircase. It takes n steps to reach the top.
+ * <p>
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ * <p>
+ * Input: n = 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ * <p>
+ * Input: n = 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ */
+public class _070_ClimbingStairs {
+
+    /**
+     * 本质上是f(i) = f(i - 1) + f(i - 2)
+     */
+    public int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        int f1 = 1, f2 = 2, result = 0;
+        for (int i = 3; i <= n; i++) {
+            result = f1 + f2;
+            f1 = f2;
+            f2 = result;
+        }
+        return result;
+    }
+}
