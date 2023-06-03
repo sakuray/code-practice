f14af038b96c852a1366cefcf56e24a355568c49
sakuray <luowentao1454@gmail.com>
2023-06-03 16:16:48 +0800

    【中】动态规划：计算字符串翻译有多少种可能

---
+++ src/main/java/com/sakuray/code/practice/leetcode/_0_99/_091_DecodeWays.java
@@ -0,0 +1,45 @@
+package com.sakuray.code.practice.leetcode._0_99;
+
+/**
+ * A message containing letters from A-Z can be encoded into numbers using the following mapping:
+ * 'A' -> "1"
+ * 'B' -> "2"
+ * ...
+ * 'Z' -> "26"
+ * To decode an encoded message, all the digits must be grouped then mapped back into letters using the reverse of the mapping above (there may be multiple ways). For example, "11106" can be mapped into:
+ *
+ * "AAJF" with the grouping (1 1 10 6)
+ * "KJF" with the grouping (11 10 6)
+ * Note that the grouping (1 11 06) is invalid because "06" cannot be mapped into 'F' since "6" is different from "06".
+ *
+ * Given a string s containing only digits, return the number of ways to decode it.
+ *
+ * The test cases are generated so that the answer fits in a 32-bit integer.
+ */
+public class _091_DecodeWays {
+
+    /**
+     * 编码有2种情况：
+     * 使用1个字符 1-9 对应A-I  f(i) = f(i-1) s(i) 不等于0
+     * 使用2个字符 10-26 对应J -Z f(i) = f(i-2) 且s(i-1)不等于0 s(i-1)s(i-2) 小于等于26
+     * f(0) = 1
+     *
+     */
+    public int numDecodings(String s) {
+        int n = s.length();
+        int[] f = new int[n + 1];
+        f[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            if (s.charAt(i - 1) != '0') {
+                f[i] += f[i - 1];
+            }
+            if (i > 1) {
+                int num =  (s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0');
+                if (num <= 26 && num >= 10) {
+                    f[i] += f[i - 2];
+                }
+            }
+        }
+        return f[n];
+    }
+}
