1cd4921527149205d9634199b69c59295d897563
sakuray <luowentao1454@gmail.com>
2023-06-03 16:33:16 +0800

    【中】回溯法：二叉搜索树的排列可能

---
+++ src/main/java/com/sakuray/code/practice/leetcode/_0_99/_095_UniqueBinarySearchTreesII.java
@@ -0,0 +1,48 @@
+package com.sakuray.code.practice.leetcode._0_99;
+
+import com.sakuray.code.practice.leetcode.Tools.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given an integer n, return all the structurally unique BST's (binary search trees),
+ * which has exactly n nodes of unique values from 1 to n. Return the answer in any order.
+ *
+ * Input: n = 3
+ * Output: [[1,null,2,null,3],[1,null,3,2],[2,1,3],[3,1,null,null,2],[3,2,null,1]]
+ *
+ * Input: n = 1
+ * Output: [[1]]
+ */
+public class _095_UniqueBinarySearchTreesII {
+
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new ArrayList<>();
+        }
+        return generateTrees(1, n);
+    }
+
+    public List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> allTrees = new ArrayList<>();
+        if (start > end) {
+            allTrees.add(null);
+            return allTrees;
+        }
+        // 遍历根节点
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftTree = generateTrees(start, i - 1);
+            List<TreeNode> rightTree = generateTrees(i + 1, end);
+            for (TreeNode left : leftTree) {
+                for (TreeNode right : rightTree) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    allTrees.add(root);
+                }
+            }
+        }
+        return allTrees;
+    }
+}
