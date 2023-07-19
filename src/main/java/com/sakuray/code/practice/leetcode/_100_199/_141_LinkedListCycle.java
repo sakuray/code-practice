ceb7127a5a798889ff15bb5d0e29baf0bcc030e7
sakuray <luowentao1454@gmail.com>
2023-07-19 19:59:42 +0800

    【简】双指针：判断链表是否有环

---
+++ src/main/java/com/sakuray/code/practice/leetcode/_100_199/_141_LinkedListCycle.java
@@ -0,0 +1,39 @@
+package com.sakuray.code.practice.leetcode._100_199;
+
+import static com.sakuray.code.practice.leetcode.Tools.*;
+
+/**
+ * Given head, the head of a linked list, determine if the linked list has a cycle in it.
+ * There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
+ * Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+ * Return true if there is a cycle in the linked list. Otherwise, return false.
+ * <p>
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+ * <p>
+ * Input: head = [1,2], pos = 0
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+ * <p>
+ * Input: head = [1], pos = -1
+ * Output: false
+ * Explanation: There is no cycle in the linked list.
+ */
+public class _141_LinkedListCycle {
+
+    /**
+     * 判断是否有环，双指针
+     */
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
